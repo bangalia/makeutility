6debedff00a69fdcd79766c0d8b1770c147efdbd
bangalia <anitabangali@yahoo.com>
2021-10-01 17:37:06 -0500

    project init

---
+++ Documents/brushwrks/app.js
@@ -0,0 +1,20 @@
+// Require Libraries
+const express = require('express');
+
+// App Setup
+const app = express();
+
+// Middleware
+const exphbs  = require('express-handlebars');
+
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+app.set('view engine', 'handlebars');
+// Routes
+app.get('/', (req, res) => {
+  res.send('Hello Squirrel');
+});
+// Start Server
+
+app.listen(3000, () => {
+  console.log('Brushwrks listening on port localhost:3000!');
+});
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# Blockchain-Tutorial
+# clubchain
