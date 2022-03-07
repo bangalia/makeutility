9516b0e80048e8867c244af9f866d390dee81309
bangalia <anitabangali@yahoo.com>
2022-03-06 23:40:33 -0600

    function finished, test begun

--- Documents/Dev/Courses/BEW 2.5/makeutility/go.mod
+++ Documents/Dev/Courses/BEW 2.5/makeutility/go.mod
@@ -1,3 +1,5 @@
 module github.com/bangalia/makeutility
 
 go 1.17
+
+
---
+++ Documents/Dev/Courses/BEW 2.5/makeutility/util_test.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"fmt"
+)
+
+func test(testing) {
+	urls := []string{
+		"https://github.com/Tech-at-DU/ACS1710-Web-Architecture",
+
+		fmt.Println("Download completed"),
+	}
+
+}
